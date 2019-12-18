58612b706e8ee44b74f5218e78142c67ba1f4fbb
massimo_giardina@hotmail.com <11910454+massimoGG@users.noreply.github.com>
2019-12-18 11:49:27 +0100

    testing netbeans

--- src/hellofx/Controller.java
+++ src/hellofx/Controller.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 public class Controller {
 
     public void initialize() {
-		//dit is een test 2.0
+		//dit is een test 2.0]
+                Michiel stinkt
+                        
     }
 }
